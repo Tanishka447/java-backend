4deefbde95b2f374947d5fbfba6dbdd89c8e61d5
Tanishka447 <Tanishka.Agarwal@adixoo.com>
2024-08-06 15:07:05 +0530

    backend

--- demo/src/main/java/com/example/ems/DemoApplication.java
+++ demo/src/main/java/com/example/ems/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.ems;
 
+// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 
 
@@ -9,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	SpringApplication.run(DemoApplication.class, args);
 
 	}
 
